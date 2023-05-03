bc6b936766d9f711d4f200942e0148a825e6cdf7
Derek Ruiz Garcia <sorerjeince93@gmail.com>
2023-05-02 21:18:09 -0700

    Derek's commit. Completed the User class. version.0.2

--- ProjectManager/src/main/java/model/User.java
+++ ProjectManager/src/main/java/model/User.java
@@ -1,4 +1,41 @@
 package model;
 
 public class User {
+
+    private String myUserName;
+
+    private String myUserEmail;
+
+    private String myPassword;
+
+    public User(String theUserName, String theUserEmail, String thePassword){
+
+        myUserName = theUserName;
+        myUserEmail = theUserEmail;
+        myPassword = thePassword;
+    }
+
+    public void setUserName(String theUsername){
+        myUserName = theUsername;
+    }
+
+    public String getMyUserName(){
+        return myUserName;
+    }
+
+    public void setUserEmail(String theUserEmail){
+        myUserEmail = theUserEmail;
+    }
+
+    public String getUserEmail(){
+        return myUserEmail;
+    }
+
+    public void setPassword(String thePassword){
+        myPassword = thePassword;
+    }
+
+    public String getMyPassword(){
+        return myPassword;
+    }
 }
