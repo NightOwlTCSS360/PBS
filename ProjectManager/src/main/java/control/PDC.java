59ec9fa806fefb5a0c4af1c9c387d9d4831c599a
Derek Ruiz Garcia <sorerjeince93@gmail.com>
2023-05-18 18:15:52 -0700

    Derek's commit. Exporting and importing feature completed in the PDC class without the file explorer version.0.51

--- ProjectManager/src/main/java/control/PDC.java
+++ ProjectManager/src/main/java/control/PDC.java
@@ -13,121 +13,200 @@ import java.util.Scanner;
 public class PDC {
     private User currentUser;
 
-    /** The file chooser that will be loaded to import or export files */
-    private JFileChooser myFileChooser;
+//    /** The file chooser that will be loaded to import or export files */
+//    private JFileChooser myFileChooser;
 
     /** A constructor for the PDC class. */
     public PDC () {
         currentUser = null;
         FlatLightLaf.setup();
-        myFileChooser = new JFileChooser();
+//        myFileChooser = new JFileChooser();
+    }
+
+    public void createSettingsFile(){
+
     }
 
     /**
      * Imports the settings located in a txt file using a File chooser. If there is already a settings file,
      * it will replace the old file with the new file that was exported. In this case the new file will contain
      * only the first 2 lines of the imported file.
-     * @param theFrame the frame on top of which the file chooser will load.
+     * @param theFileToImport that will be imported into the program.
      * @author Derek J. Ruiz Garcia
      */
-    public void importSettings(JFrame theFrame) throws IOException {
-        int result = myFileChooser.showDialog(theFrame, "Select settings file to import");
+    public void importSettings(File theFileToImport) throws IOException {
+        String fileName = theFileToImport.getName();
+//        String fileType = fileName.substring(fileName.length() - 3, fileName.length());
 
-        if (result == JFileChooser.APPROVE_OPTION){
-            String fileName = myFileChooser.getSelectedFile().getName();
-            String fileType = fileName.substring(fileName.length() - 3, fileName.length());
+//        if (fileType.equals("txt")){
+        final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
+        File settingsFile = new File(settingsLocation);
 
-            if (fileType.equals("txt")){
-                final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
-                File settingsFile = new File(settingsLocation);
-
-                try {
-                    boolean fileAlreadyExists = settingsFile.createNewFile();
-                    if (fileAlreadyExists){
-                        System.out.println("File already exists!");
-                        settingsFile.delete();
-                        System.out.println("Has been deleted!");
-                        settingsFile.createNewFile();
-                        System.out.println("File has been re-created");
-                    } else {
-                        System.out.println("File was created!");
-                    }
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
-                //Because now we have a file we can write to a file.
-                File chosenFile = myFileChooser.getSelectedFile();
-                System.out.println("File has been selected!: " + chosenFile.getName());
-                Scanner sc = new Scanner(chosenFile);
-
-                int n = 0;                      // this is the number of lines a settings file should have (name, email)
-                try (BufferedWriter fileW = new BufferedWriter(new FileWriter(settingsLocation))) {
-                    while(sc.hasNextLine() && n < 2) {
-                        fileW.write(sc.nextLine());
-                        fileW.newLine();
-                        n++;
-                    }
-                    fileW.close();
-                } catch(IOException e) {
-                    e.printStackTrace();
-                }
+        try {
+            boolean fileAlreadyExists = settingsFile.createNewFile();
+            if (fileAlreadyExists){
+                System.out.println("File already exists!");
+                settingsFile.delete();
+                System.out.println("Has been deleted!");
+                settingsFile.createNewFile();
+                System.out.println("File has been re-created");
             } else {
-                JOptionPane.showMessageDialog(theFrame, "The selected file is not a txt file!",
-                        "Error!", JOptionPane.ERROR_MESSAGE);
+                System.out.println("File was created!");
             }
+        } catch (IOException e){
+            e.printStackTrace();
         }
+                //Because now we have a file we can write to a file.
+        System.out.println("File has been selected!: " + theFileToImport.getName());
+        Scanner sc = new Scanner(theFileToImport);
+
+        int n = 0;                      // this is the number of lines a settings file should have (name, email)
+        try (BufferedWriter fileW = new BufferedWriter(new FileWriter(settingsLocation))) {
+            while(sc.hasNextLine() && n < 2) {
+                fileW.write(sc.nextLine());
+                fileW.newLine();
+                n++;
+            }
+            fileW.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+//            } else {
+//                JOptionPane.showMessageDialog(theFrame, "The selected file is not a txt file!",
+//                        "Error!", JOptionPane.ERROR_MESSAGE);
+//            }
+
+//        int result = myFileChooser.showDialog(theFrame, "Select settings file to import");
+//
+//        if (result == JFileChooser.APPROVE_OPTION){
+//            String fileName = myFileChooser.getSelectedFile().getName();
+//            String fileType = fileName.substring(fileName.length() - 3, fileName.length());
+//
+//            if (fileType.equals("txt")){
+//                final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
+//                File settingsFile = new File(settingsLocation);
+//
+//                try {
+//                    boolean fileAlreadyExists = settingsFile.createNewFile();
+//                    if (fileAlreadyExists){
+//                        System.out.println("File already exists!");
+//                        settingsFile.delete();
+//                        System.out.println("Has been deleted!");
+//                        settingsFile.createNewFile();
+//                        System.out.println("File has been re-created");
+//                    } else {
+//                        System.out.println("File was created!");
+//                    }
+//                } catch (IOException e){
+//                    e.printStackTrace();
+//                }
+//                //Because now we have a file we can write to a file.
+//                File chosenFile = myFileChooser.getSelectedFile();
+//                System.out.println("File has been selected!: " + chosenFile.getName());
+//                Scanner sc = new Scanner(chosenFile);
+//
+//                int n = 0;                      // this is the number of lines a settings file should have (name, email)
+//                try (BufferedWriter fileW = new BufferedWriter(new FileWriter(settingsLocation))) {
+//                    while(sc.hasNextLine() && n < 2) {
+//                        fileW.write(sc.nextLine());
+//                        fileW.newLine();
+//                        n++;
+//                    }
+//                    fileW.close();
+//                } catch(IOException e) {
+//                    e.printStackTrace();
+//                }
+//            } else {
+//                JOptionPane.showMessageDialog(theFrame, "The selected file is not a txt file!",
+//                        "Error!", JOptionPane.ERROR_MESSAGE);
+//            }
+
     }
 
     /**
      * Exports the settings using a File chooser to indicate the location to store the settings as a txt file.
      * Note: if a settings file already exist in the location selected it will overwrite it without asking.
-     * @param theFrame the frame on top of which the file chooser will load.
+     * @param theFile that will be used to retrieve the location where the exported settings file will be stored.
      * @throws FileNotFoundException if there is not a Settings file inside the project.
      * @author Derek J. Ruiz Garcia
      */
-    public void exportSettings(JFrame theFrame) throws FileNotFoundException {
-        int result = myFileChooser.showDialog(theFrame, "Select location");
+    public void exportSettings(File theFile) throws FileNotFoundException {
+        final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
+        File localSettingsFile = new File(settingsLocation);
+        final String exportingLocation = theFile.getPath();
 
-        if (result == JFileChooser.APPROVE_OPTION){
-            final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
-            File localSettingsFile = new File(settingsLocation);
-
-            final String exportingLocation = myFileChooser.getSelectedFile().getPath();
-
-            // This showed us that it will provide the right path!
-//            String thePath = myFileChooser.getSelectedFile().getPath();
-//            System.out.println("The path chosen: " + thePath);
-            File exportedSettingsFile = new File(exportingLocation);
-
-            try {
-                boolean fileAlreadyExists = exportedSettingsFile.createNewFile();
-                if (fileAlreadyExists){
-                    System.out.println("File already exists!");
-                    exportedSettingsFile.delete();
-                    System.out.println("Has been deleted!");
-                    exportedSettingsFile.createNewFile();
-                    System.out.println("File has been re-created");
-                } else {
-                    System.out.println("File was created!");
-                }
-            } catch (IOException e){
-                e.printStackTrace();
+        File exportedSettingsFile = new File(exportingLocation);
+        try {
+            boolean fileAlreadyExists = exportedSettingsFile.createNewFile();
+            if (fileAlreadyExists){
+                System.out.println("File already exists!");
+                exportedSettingsFile.delete();
+                System.out.println("Has been deleted!");
+                exportedSettingsFile.createNewFile();
+                System.out.println("File has been re-created");
+            } else {
+                System.out.println("File was created!");
             }
-            // the local settings file
-            Scanner sc = new Scanner(localSettingsFile);
-
-            int n = 0;                      // this is the number of lines a settings file should have (name, email)
-            try (BufferedWriter fileW = new BufferedWriter(new FileWriter(exportingLocation))) {
-                while(sc.hasNextLine() && n < 2) {
-                    fileW.write(sc.nextLine());
-                    fileW.newLine();
-                    n++;
-                }
-                fileW.close();
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
+        } catch (IOException e){
+            e.printStackTrace();
         }
+        // the local settings file
+        Scanner sc = new Scanner(localSettingsFile);
+
+        int n = 0;                      // this is the number of lines a settings file should have (name, email)
+        try (BufferedWriter fileW = new BufferedWriter(new FileWriter(exportingLocation))) {
+            while(sc.hasNextLine() && n < 2) {
+                fileW.write(sc.nextLine());
+                fileW.newLine();
+                n++;
+            }
+            fileW.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+//        int result = myFileChooser.showDialog(theFrame, "Select location");
+//
+//        if (result == JFileChooser.APPROVE_OPTION){
+//            final String settingsLocation = "ProjectManager\\src\\main\\resources\\Settings\\Settings.txt";
+//            File localSettingsFile = new File(settingsLocation);
+//
+//            final String exportingLocation = myFileChooser.getSelectedFile().getPath();
+//
+//            // This showed us that it will provide the right path!
+////            String thePath = myFileChooser.getSelectedFile().getPath();
+////            System.out.println("The path chosen: " + thePath);
+//            File exportedSettingsFile = new File(exportingLocation);
+//
+//            try {
+//                boolean fileAlreadyExists = exportedSettingsFile.createNewFile();
+//                if (fileAlreadyExists){
+//                    System.out.println("File already exists!");
+//                    exportedSettingsFile.delete();
+//                    System.out.println("Has been deleted!");
+//                    exportedSettingsFile.createNewFile();
+//                    System.out.println("File has been re-created");
+//                } else {
+//                    System.out.println("File was created!");
+//                }
+//            } catch (IOException e){
+//                e.printStackTrace();
+//            }
+//            // the local settings file
+//            Scanner sc = new Scanner(localSettingsFile);
+//
+//            int n = 0;                      // this is the number of lines a settings file should have (name, email)
+//            try (BufferedWriter fileW = new BufferedWriter(new FileWriter(exportingLocation))) {
+//                while(sc.hasNextLine() && n < 2) {
+//                    fileW.write(sc.nextLine());
+//                    fileW.newLine();
+//                    n++;
+//                }
+//                fileW.close();
+//            } catch(IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     public void setCurrentUser( final User theUser) {
--- ProjectManager/src/main/java/test/ImportExportSettingsTester.java
+++ ProjectManager/src/main/java/test/ImportExportSettingsTester.java
@@ -11,12 +11,16 @@ public class ImportExportSettingsTester {
         JFrame myFrame = new JFrame();
         PDC myPDC = new PDC();
 
-        // Comment/uncomment this to test for the import.
-//        myPDC.importSettings(myFrame);
+        JFileChooser fc = new JFileChooser();
+//        int result = fc.showDialog(myFrame, "choose a file to import");
+
+//        //         Comment/uncomment this to test for the import.
+//        myPDC.importSettings(fc.getSelectedFile());
 //        myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         // Comment/uncomment this to test for the export
-        myPDC.exportSettings(myFrame);
+        int result = fc.showDialog(myFrame, "Choose a location to export settings");
+        myPDC.exportSettings(fc.getSelectedFile());
         myFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
